04004e02700f46e29393e866df39bd006f904262
SimpleGen <45266498+SimpleGen@users.noreply.github.com>
2019-02-01 23:15:49 +0100

    Update chatclear.py

--- chatclear.py
+++ chatclear.py
@@ -12,25 +12,13 @@ bot = commands.Bot(command_prefix=prefix)
 
 @bot.event
 async def on_ready():
-    print("Höher gewinnt")
+    print("ONLINE")
 
 @bot.event
 async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name="Member")
     await bot.add_roles(member, role)
-
-@bot.command(pass_context=True)
-async def start(ctx):
-    await bot.say(random.randint(0, 10000))
-
-@bot.command(pass_context=True)
-async def clear(ctx, amount=100):
-    channel = ctx.message.channel
-    messages = []
-    async for message in bot.logs_from(channel, limit=int(amount) + 1):
-        messages.append(message)
-    await bot.delete_messages(messages)
-    await bot.say("**DELETED**")
+    
 
 bot.run("NTM2MTQwNDIwNDI2NDk4MDc1.DySXNw.aTZWvdkLPmIUSEgdn2qOO91iYdo")
 
